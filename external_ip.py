b42c61b6008dcaa6224fe1cd0edc26881d4c1d72
Tor Arve Stangeland <tor.arve.stangeland@gmail.com>
2013-05-09 11:25:18 +0200

    Small script to get external ip using http://checkip.dyndns.com/

---
+++ external_ip.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Sample script to check external IP using http://checkip.dyndns.com/
+
+try:
+    import urllib.request as urllib2
+except:
+    import urllib2
+
+import re
+
+URL = "http://checkip.dyndns.com/"
+
+def get_external_ip():
+    res = urllib2.urlopen(URL)
+    ex = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", res.read())
+    return ex[0]
+
+
+if __name__=="__main__":
+    print("External IP: %s" % get_external_ip())
